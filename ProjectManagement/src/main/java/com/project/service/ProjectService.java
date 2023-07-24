76eb98b5b30aec1531707cec71b995080b86f7df
shakambhari <shakambharihift36@gmail.com>
2023-07-24 13:03:33 +0530

    addeD

--- ProjectManagement/src/main/java/com/project/service/ProjectService.java
+++ ProjectManagement/src/main/java/com/project/service/ProjectService.java
@@ -1,5 +1,21 @@
 package com.project.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.model.Project;
+import com.project.repository.ProjectRepository;
+
+@Service
 public class ProjectService {
 
+	
+	@Autowired
+	private ProjectRepository projectRepo;
+	
+	public String addEmployee(Project project)
+	{
+		projectRepo.save(project);
+		return("Project added sucessfully");
+	}
 }
